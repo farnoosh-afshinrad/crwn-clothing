0022487fb4efa8e689896c39a96f2d7e9865d910
farnoosh rad <radfarnush@gmail.com>
2022-02-15 17:41:31 +0330

    added header and linked it to the homepage and shope page and contact one

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import { Route, Switch } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shoppage.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/shop" component={ShopPage} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
   );
 }
 
